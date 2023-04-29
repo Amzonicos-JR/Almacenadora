251ffe296eeb2b61de731e2afc364398b8c65408
agutierrez-2021188 <agutierrez-2021188>
2023-04-29 17:41:23 -0600

    Se solucionarion errores del merge anterior

--- almacenadora/configs/app.js
+++ almacenadora/configs/app.js
@@ -10,35 +10,11 @@ const cors = require('cors');
 //Instancia de express
 const app = express();
 const port = process.env.PORT || 3500;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-//<<<<<<< HEAD
->>>>>>> jperez-2018495
+
 const accountRoutes = require('../src/account/account.routes');
-//=======
 const userRoutes = require('../src/user/user.routes')
 const serviceRoutes = require('../src/additional services/additionalServices.routes')
-
-<<<<<<< HEAD
->>>>>>> asumpango-2018373
-=======
-const accountRoutes = require('../src/account/account.routes');
 const cellarRoutes = require('../src/cellar/cellar.routes');
->>>>>>> dsalazar-2021181
-=======
-//>>>>>>> asumpango-2018373
->>>>>>> jperez-2018495
-=======
-
-//=======
-const userRoutes = require('../src/user/user.routes')
-const serviceRoutes = require('../src/additional services/additionalServices.routes')
-const accountRoutes = require('../src/account/account.routes');
-const cellarRoutes = require('../src/cellar/cellar.routes');
->>>>>>> asumpango-2018373
 
 //CONFIGURAR EL SERVIDOR HTTP DE EXPRESS
 app.use(express.urlencoded({extended: false}));
@@ -46,34 +22,12 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-//<<<<<<< HEAD
->>>>>>> jperez-2018495
-=======
 
->>>>>>> asumpango-2018373
-app.use('/account', accountRoutes);
-//=======
 app.use('/user', userRoutes);
-<<<<<<< HEAD
->>>>>>> asumpango-2018373
-=======
 app.use('/account', accountRoutes);
 app.use('/cellar', cellarRoutes);
->>>>>>> dsalazar-2021181
-=======
 app.use('/service', serviceRoutes)
-//>>>>>>> asumpango-2018373
->>>>>>> jperez-2018495
-
-app.use('/account', accountRoutes);
 app.use('/cellar', cellarRoutes);
-
-app.use('/service', serviceRoutes)
 
 //Función donde se levanta el servidor
 exports.initServer = ()=>{
--- almacenadora/index.js
+++ almacenadora/index.js
@@ -3,15 +3,8 @@
 require('dotenv').config()
 const mongoConfig = require('./configs/mongo');
 const app = require('./configs/app');
-<<<<<<< HEAD
-
-
-mongoConfig.connect();
-app.initServer();
-=======
 const userController = require('./src/user/user.controller')
 
 mongoConfig.connect();
 app.initServer();
 userController.userDefault();
->>>>>>> asumpango-2018373
